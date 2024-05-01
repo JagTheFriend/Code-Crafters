d291e862926821e2f2fe81099f6abbffd9c8f401
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-01 17:03:08 +0200

    Updated config
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         "ui-text": "#f3eae9",
         "ui-background": "#1a1a1a",
