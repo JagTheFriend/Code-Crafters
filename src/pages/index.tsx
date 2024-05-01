d9e84a0a67b171ae733090a2f8aa35addba22d3b
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-01 17:11:22 +0200

    Use spotlight component
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
 import Head from "next/head";
 import Navbar from "~/components/Navbar";
+import { Spotlight } from "~/components/ui/Spotlight";
+
+function SpotlightName() {
+  return (
+    <div className="bg-grid-white/[0.02] relative flex h-[40rem] w-full overflow-hidden rounded-md antialiased md:items-center md:justify-center">
+      <Spotlight
+        className="-top-40 left-0 md:-top-20 md:left-60"
+        fill="white"
+      />
+      <div className=" relative z-10  mx-auto w-full max-w-7xl  p-4 pt-20 md:pt-0">
+        <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+          Code Crafters
+        </h1>
+        <p className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300">
+          Inspiring Minds, One Post at a Time
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
@@ -11,6 +31,7 @@ export default function Home() {
       </Head>
       <main>
         <Navbar />
+        <SpotlightName />
       </main>
     </>
   );
