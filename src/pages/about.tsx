87361e87072be09dffc53299e3e63de83788f3c3
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-02 10:45:04 +0200

    Working on new page
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function AboutPage() {
+  return (
+    <>
+      <Head>
+        <title>About Code Crafters</title>
+        <meta name="description" content="About Code Crafters" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function ContactPage() {
+  return (
+    <>
+      <Head>
+        <title>Contact</title>
+        <meta name="description" content="Contact author of Code Crafters" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
